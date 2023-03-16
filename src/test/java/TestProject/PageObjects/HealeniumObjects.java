0447dd13bd7f52e96859d8e0d9f224f6cab0832d
mujtaba.afzal <mujtaba.afzal@10pearls.com>
2023-03-16 12:55:48 +0500

    healenium changes added

--- src/test/java/TestProject/PageObjects/HealeniumObjects.java
+++ src/test/java/TestProject/PageObjects/HealeniumObjects.java
@@ -8,40 +8,52 @@ import java.util.List;
 
 public class HealeniumObjects {
     WebDriver driver;
-    public HealeniumObjects(WebDriver driver){
+
+    public HealeniumObjects(WebDriver driver) {
         this.driver = driver;
     }
-    public WebElement getSubmitButton(){
+
+    public WebElement getSubmitButton() {
         return driver.findElement(By.xpath("//button[@id='Submit']"));
     }
-    public WebElement getByID(){
+
+    public WebElement getByID() {
         return driver.findElement(By.id("change_id"));
     }
-    public WebElement getByClassName(){
+
+    public WebElement getByClassName() {
         return driver.findElement(By.className("test_class"));
     }
-    public WebElement getByName(){
+
+    public WebElement getByName() {
         return driver.findElement(By.name("change_name"));
     }
-    public WebElement getByLinkText(){
+
+    public WebElement getByLinkText() {
         return driver.findElement(By.linkText("Change: LinkText, PartialLinkText"));
     }
-    public WebElement getByTagName(){
+
+    public WebElement getByTagName() {
         return driver.findElement(By.tagName("test_tag"));
     }
-    public WebElement getByXpath(String xpath){
+
+    public WebElement getByXpath(String xpath) {
         return driver.findElement(By.xpath(xpath));
     }
-    public List<WebElement> getByXpathCheckbox(){
+
+    public List<WebElement> getByXpathCheckbox() {
         return driver.findElements(By.xpath("//div[@class='test-form healenium-form validate-form']/div[@class='wrap-input1 validate-input']/input[@type='checkbox']"));
     }
-    public WebElement getSubmitButtonCheckbox(){
+
+    public WebElement getSubmitButtonCheckbox() {
         return driver.findElement(By.id("Submit_checkbox"));
     }
-    public WebElement getByCssSelectorCheckbox(int index){
-        return driver.findElement(By.cssSelector("input#form_checked"+index+".input1"));
+
+    public WebElement getByCssSelectorCheckbox(int index) {
+        return driver.findElement(By.cssSelector("input#form_checked" + index + ".input1"));
     }
-    public boolean clickFirstCheckbox(){
+
+    public boolean clickFirstCheckbox() {
         this.getByCssSelectorCheckbox(1).click();
         return true;
     }
--- src/test/java/TestProject/Tests/HealeniumTests.java
+++ src/test/java/TestProject/Tests/HealeniumTests.java
@@ -2,129 +2,129 @@ package TestProject.Tests;
 
 import TestProject.PageObjects.HealeniumObjects;
 import com.epam.healenium.SelfHealingDriver;
-import com.epam.healenium.annotation.DisableHealing;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
-import utils.UtilsConfig;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import java.util.Properties;
-
-public class Healenium {
+public class HealeniumTests {
     WebDriver delegate;
     SelfHealingDriver driver;
-    Properties properties;
     HealeniumObjects pageObjects;
+
     @BeforeClass
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\mujtaba.afzal\\Documents\\ChromeDriver\\chromedriver.exe");
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mujtaba.afzal\\Documents\\ChromeDriver\\chromedriver.exe");
         delegate = new ChromeDriver();
         //create Self-healing driver
         driver = SelfHealingDriver.create(delegate);
-        properties = UtilsConfig.getProperties("SiteData");
-        driver.get(properties.getProperty("HLM_URL"));
         pageObjects = new HealeniumObjects(driver);
     }
+
     @BeforeMethod
-    public void beforeEach(){
-        driver.get(properties.getProperty("HLM_URL"));
+    public void beforeEach() {
+        driver.get("https://elenastepuro.github.io/test_env/index.html");
     }
+
     @Test(description = "Change in id")
-    public void testChangeId(){
-        if(pageObjects.getByID()!=null){
+    public void testChangeId() {
+        if (pageObjects.getByID() != null) {
             System.out.println("Case 1: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByID()!=null){
+        if (pageObjects.getByID() != null) {
             System.out.println("Case 1: Element Found After Healing");
         }
     }
+
     @Test(description = "Change in ClassName")
-    public void testChangeClassName(){
-        if(pageObjects.getByClassName()!=null){
+    public void testChangeClassName() {
+        if (pageObjects.getByClassName() != null) {
             System.out.println("Case 2: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByClassName()!=null){
+        if (pageObjects.getByClassName() != null) {
             System.out.println("Case 2: Element Found After Healing");
         }
     }
 
     @Test(description = "Change in Name")
-    public void testChangeName(){
-        if(pageObjects.getByName()!=null){
+    public void testChangeName() {
+        if (pageObjects.getByName() != null) {
             System.out.println("Case 3: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByName()!=null){
+        if (pageObjects.getByName() != null) {
             System.out.println("Case 3: Element Found After Healing");
         }
     }
 
     @Test(description = "Change in Link Text")
-    public void testChangeLinkText(){
-        if(pageObjects.getByLinkText()!=null){
+    public void testChangeLinkText() {
+        if (pageObjects.getByLinkText() != null) {
             System.out.println("Case 4: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByLinkText()!=null){
+        if (pageObjects.getByLinkText() != null) {
             System.out.println("Case 4: Element Found After Healing");
         }
     }
 
     @Test(description = "Change in Tag Name")
-    public void testChangeTagName(){
-        if(pageObjects.getByTagName()!=null){
+    public void testChangeTagName() {
+        if (pageObjects.getByTagName() != null) {
             System.out.println("Case 5: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByTagName()!=null){
+        if (pageObjects.getByTagName() != null) {
             System.out.println("Case 5: Element Found After Healing");
         }
     }
 
     @Test(description = "Change in xpath")
-    public void testChangeXpath(){
-        if(pageObjects.getByXpath("//*[@id='change:name']")!=null){
+    public void testChangeXpath() {
+        if (pageObjects.getByXpath("//*[@id='change:name']") != null) {
             System.out.println("Case 6: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByXpath("//*[@id='change:name']")!=null){
+        if (pageObjects.getByXpath("//*[@id='change:name']") != null) {
             System.out.println("Case 6: Element Found After Healing");
         }
     }
 
     @Test(description = "XPath Contains")
-    public void testXpathContains(){
-        if(pageObjects.getByXpath("//input[contains(@class, 'test')]")!=null){
+    public void testXpathContains() {
+        if (pageObjects.getByXpath("//input[contains(@class, 'test')]") != null) {
             System.out.println("Case 7: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByXpath("//input[contains(@class, 'test')]")!=null){
+        if (pageObjects.getByXpath("//input[contains(@class, 'test')]") != null) {
             System.out.println("Case 7: Element Found After Healing");
         }
     }
 
     @Test(description = "XPath Ancestor::")
-    public void testXPathAncestor(){
-        if(pageObjects.getByXpath("(//*[starts-with(@class, 'test')]/ancestor::div[@class='healenium-form validate-form']//input)[1]")!=null){
+    public void testXPathAncestor() {
+        if (pageObjects.getByXpath("(//*[starts-with(@class, 'test')]/ancestor::div[@class='healenium-form validate-form']//input)[1]") != null) {
             System.out.println("Case 8: Element Found");
         }
         pageObjects.getSubmitButton().click();
-        if(pageObjects.getByXpath("(//*[starts-with(@class, 'test')]/ancestor::div[@class='healenium-form validate-form']//input)[1]")!=null){
+        if (pageObjects.getByXpath("(//*[starts-with(@class, 'test')]/ancestor::div[@class='healenium-form validate-form']//input)[1]") != null) {
             System.out.println("Case 8: Element Found After Healing");
         }
     }
 
     @Test(description = "XPath Ancestor::")
     //@DisableHealing
-    public void testCheckBoxes(){
-        if(pageObjects.clickFirstCheckbox()){
-            Assert.assertFalse(pageObjects.getByCssSelectorCheckbox(1).isSelected(),"Case 9: Element Found");
+    public void testCheckBoxes() {
+        if (pageObjects.clickFirstCheckbox()) {
+            Assert.assertFalse(pageObjects.getByCssSelectorCheckbox(1).isSelected(), "Case 9: Element Found");
         }
         pageObjects.getSubmitButtonCheckbox().click();
-        if(pageObjects.clickFirstCheckbox()){
+        if (pageObjects.clickFirstCheckbox()) {
             Assert.assertTrue(pageObjects.getByCssSelectorCheckbox(1).isSelected(), "Case 9: Element Found After Healing");
         }
     }
@@ -132,5 +132,6 @@ public class Healenium {
     @AfterClass
     public void teardown() {
         driver.quit();
+
     }
 }
